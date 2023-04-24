11db386dc036d95e7e3f052ad739afb3ac9dd025
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-04-24 15:29:18 +0100

    1529

--- rundeck-cid00998-push-sims-reportdef-scomis.ps1
+++ rundeck-cid00998-push-sims-reportdef-scomis.ps1
@@ -1,21 +1,21 @@
 Clear-Host
 
-$CID = "@option.ChangeID@"
-$GamDir = "D:\AppData\GAMXTD3\app"
-$DataDir =" D:\AppData\Rundeck\$CID\Data"
-$LogDir = "D:\AppData\Rundeck\$CID\Logs"
-$transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
-$tempcsv = "$DataDir\temp.csv"
-$tempcsv2 = "$DataDir\temp2.csv"
-$now = $(Get-Date -Format "dd MMMM yyyy HHHH:mm:s")
-$SimsPWD = "@option.SIMSuserPASS@" #rundeck key vault
+#$CID = "@option.ChangeID@"
+#$GamDir = "D:\AppData\GAMXTD3\app"
+#$DataDir =" D:\AppData\Rundeck\$CID\Data"
+#$LogDir = "D:\AppData\Rundeck\$CID\Logs"
+#$transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
+#$tempcsv = "$DataDir\temp.csv"
+#$tempcsv2 = "$DataDir\temp2.csv"
+#$now = $(Get-Date -Format "dd MMMM yyyy HHHH:mm:s")
+#$SimsPWD = "@option.SIMSuserPASS@" #rundeck key vault
 
 $SimsReport = "@option.SimsReport@" # sims report to download push
 $SimsReportUser = "@option.SimsReportUser@" # sims user to get/push report def
 $GoogleDocIDsimsInstances = "@option.GoogleDocIDsimsInstances@" #ghseet containing all instances to be processed
 $GoogleDocReportSourceID = "@option.GoogleDocReportSourceID@" #exported/uploaded sims report definition
 
-$SimsInstancesCSV = "$datadir\@option.SimsInstancesCSV@.csv"
+#$SimsInstancesCSV = "$datadir\@option.SimsInstancesCSV@.csv"
 $GoogleGamMail = "@option.GoogleGamMail@"
 
 Clear-Host
