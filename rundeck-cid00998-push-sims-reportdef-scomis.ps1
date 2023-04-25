a6448496a013ee95166ea9af8b9afc519a093f7d
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-04-25 22:46:42 +0100

    2246

--- rundeck-cid00998-push-sims-reportdef-scomis.ps1
+++ rundeck-cid00998-push-sims-reportdef-scomis.ps1
@@ -1,35 +1,6 @@
 Clear-Host
-
-#$CID = "@option.ChangeID@"
-#$GamDir = "D:\AppData\GAMXTD3\app"
-#$DataDir =" D:\AppData\Rundeck\$CID\Data"
-#$LogDir = "D:\AppData\Rundeck\$CID\Logs"
-#$transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
-#$tempcsv = "$DataDir\temp.csv"
-#$tempcsv2 = "$DataDir\temp2.csv"
-#$now = $(Get-Date -Format "dd MMMM yyyy HHHH:mm:s")
-#$SimsPWD = "@option.SIMSuserPASS@" #rundeck key vault
-
-#$SimsReport = "@option.SimsReport@" # sims report to download push
-#$SimsReportUser = "@option.SimsReportUser@" # sims user to get/push report def
-#$GoogleDocIDsimsInstances = "@option.GoogleDocIDsimsInstances@" #ghseet containing all instances to be processed
-#$GoogleDocReportSourceID = "@option.GoogleDocReportSourceID@" #exported/uploaded sims report definition
-
-#$SimsInstancesCSV = "$datadir\@option.SimsInstancesCSV@.csv"
-#$GoogleGamMail = "@option.GoogleGamMail@"
-
-Clear-Host
 Get-Variable | format-table -Wrap -Autosize
 
-$AutomaticVariables = Get-Variable
-function cmpv {
-    Compare-Object (Get-Variable) $AutomaticVariables -Property Name -PassThru | Where -Property Name -ne "AutomaticVariables"
-}
-
-cmpv
-
-
-#Write-Host "Downloading Googlesheet containing all sims instances, instance name etc..."
 Write-Host "Passed vars..."
 $SimsInstancesCSV
 $SimsReport
@@ -37,4 +8,46 @@ $GoogleDocIDsimsInstances
 $GoogleGamMail
 $SimsReportUser
 
-#Stop-Transcript
+Write-Host "Downloading Googlesheet containing all sims instances, instance name etc..."
+
+if (Test-Path -path $SimsInstancesCSV ) {
+
+	Write-Host "$SimsInstancesCSV exists, deleting..."
+	Remove-Item -Path $SimsInstancesCSV -Force
+}
+
+Set-Location $GamDir
+
+Invoke-Expression "$GamDir\gam.exe user $GoogleGamMail get drivefile id $GoogleDocIDsimsInstances format csv targetfolder $datadir" -ErrorAction SilentlyContinue
+Start-Sleep 2
+$simsinstances = Import-Csv -Path $SimsInstancesCSV
+
+#loop through each sims instance
+foreach ($sims in $simsinstances) {
+    write-host "------------------------------------------------------------------"
+	write-host "IP      :" $sims.ip
+    write-host "MSSQL   :" $sims.SQLinstance
+    Write-Host "Sims DB :" $sims.dbname
+    write-host "GdocID  :" $sims.GoogleDocID
+    Write-Host "School  :" $sims.School
+    Write-Host "DfE num :" $sims.DFEnumber
+
+	$now = $(Get-Date -Format "dd MMMM yyyy HHHH:mm:s")
+	$simsServerName = "$($sims.ip)\$($sims.SQLInstance)"
+	$SimsDatabaseName = "$($sims.dbname)"
+	$simsSchool = "$($sims.school)"
+	#$GoogleDocTitle = "DSX Attendance - $simsSchool : StartDate:$XMLdateStart EndDate:$XMLdateEnd ReportRuntime: $now"
+	$GoogleDocID = "$($sims.GoogleDocID)"
+	$simsDFE = "$($sims.DFEnumber)"	
+
+}
+
+
+<#
+$AutomaticVariables = Get-Variable
+function cmpv {
+    Compare-Object (Get-Variable) $AutomaticVariables -Property Name -PassThru | Where -Property Name -ne "AutomaticVariables"
+}
+
+cmpv
+#>
