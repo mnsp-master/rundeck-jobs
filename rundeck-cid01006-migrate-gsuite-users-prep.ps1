75cd328c30361d779a9ea428d34dfbf7194a04df
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-04-27 12:54:09 +0100

    1254

--- rundeck-cid01006-migrate-gsuite-users-prep.ps1
+++ rundeck-cid01006-migrate-gsuite-users-prep.ps1
@@ -6,16 +6,16 @@ Write-Host "MNSP Version: $mnspVer"
 
 #$csv = "C:\temp\users.csv"
 #$migratedDomain = "dev.mnsp.org.uk"
-
+$SourceGsuiteOrgUnits = @("$SourceGsuiteOrgUnit1","$SourceGsuiteOrgUnit2")
 set-location $GamDir
 
-invoke-expression ".\gam.exe ou_and_children 'staff/Non-Teaching Staff/men' print allfields  | out-file $tempcsv"
 
-$users =@()
-$users = import-csv -Path $tempcsv | Where-Object { $_.suspended -notlike "True" } #exclude any suspended accounts
+invoke-expression ".\gam.exe ou_and_children 'staff' print allfields  | out-file $tempcsv" #produce csv header
+
+$users = @()
+$users = import-csv -Path $tempcsv | Where-Object { $_.suspended -notlike "True" } AND { $_.primaryEmail -like "mendipstudioschool.org.uk"} #exclude any suspended accounts
 
 Write-host "Number of source users to process..." $users.count
-
 
 foreach ($user in $users) {
 
@@ -26,5 +26,5 @@ $migratedUser
 } 
 
 
-$SourceGsuiteOrgUnits = @("$SourceGsuiteOrgUnit1","$SourceGsuiteOrgUnit2")
+
 $SourceGsuiteOrgUnits
