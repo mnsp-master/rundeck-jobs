fdce18f855fe349f5540f3fd2817fb020d4ec1d3
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-04-27 10:38:37 +0100

    1038

--- rundeck-cid01006-migrate-gsuite-users-prep.ps1
+++ rundeck-cid01006-migrate-gsuite-users-prep.ps1
@@ -1,5 +1,5 @@
 Clear-Host
-$mnspVer = "0.0.0.0.5"
+$mnspVer = "0.0.0.0.6"
 
 Write-Host "MNSP Version: $mnspVer"
 #Get-Variable | format-table -Wrap -Autosize
@@ -13,7 +13,6 @@ invoke-expression ".\gam.exe ou_and_children 'staff/Non-Teaching Staff/men' prin
 
 $users =@()
 $users = import-csv -Path $tempcsv | Where-Object { $_.suspended -notlike "True" } #exclude any suspended accounts
-$users.suspended
 
 Write-host "Number of source users to process..." $users.count
 
@@ -25,3 +24,5 @@ $migratedUser =  "$username@$migratedDomain"
 $migratedUser
 
 } 
+
+$SourceGsuiteOrgUnits
