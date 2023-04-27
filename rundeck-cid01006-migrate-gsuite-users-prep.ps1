c031efa74cee70d5226cd5ea8bd4d50b6d385145
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-04-27 13:02:12 +0100

    1302

--- rundeck-cid01006-migrate-gsuite-users-prep.ps1
+++ rundeck-cid01006-migrate-gsuite-users-prep.ps1
@@ -1,5 +1,5 @@
 Clear-Host
-$mnspVer = "0.0.0.1.0"
+$mnspVer = "0.0.0.1.1"
 
 Write-Host "MNSP Version: $mnspVer"
 #Get-Variable | format-table -Wrap -Autosize
@@ -13,7 +13,7 @@ set-location $GamDir
 invoke-expression ".\gam.exe ou_and_children 'staff' print allfields  | out-file $tempcsv" #produce csv header
 
 $users = @()
-$users = import-csv -Path $tempcsv | Where-Object { $_.suspended -notlike "True" } AND { $_.primaryEmail -like "mendipstudioschool.org.uk"} #exclude any suspended accounts
+$users = import-csv -Path $tempcsv | Where-Object { ($_.suspended -notlike "True") -and ($_.primaryEmail -like "*mendipstudioschool.org.uk")} #exclude any suspended accounts, include only @mendipstudioschool.org.uk 
 
 Write-host "Number of source users to process..." $users.count
 
