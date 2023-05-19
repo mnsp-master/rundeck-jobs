939d557238f16224522427a95a666ead30c0f657
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-05-19 12:40:31 +0100

    12:40

--- rundeck-cid01047-scheduled-sims-reports-wri.ps1
+++ rundeck-cid01047-scheduled-sims-reports-wri.ps1
@@ -15,6 +15,7 @@ Write-Host "Downloading Googlesheet containing all sims report(s) info, instance
     Invoke-Expression "$GamDir\gam.exe user $GoogleGamMail get drivefile id $SimsReportsSourceCSVGsheetID format csv targetfolder $datadir" -ErrorAction SilentlyContinue
 Start-Sleep 2
 $SimsReportDefs = Import-Csv -Path $SimsReportsSourceCSV
+Get-Variable
 
 Write-Host "loop through each Sims ReportDef..."
 
