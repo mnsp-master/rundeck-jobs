2f7ba8c69345cdaeeabc96fca2131d3b624fced8
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-06-12 11:01:25 +0100

    1100

--- rundeck-cid01090-cleanup-pars-backups.ps1
+++ rundeck-cid01090-cleanup-pars-backups.ps1
@@ -1,10 +1,12 @@
 Clear-host
 
-$mnspVer = "1.0.0.0.6"
+$mnspVer = "1.0.0.0.7"
+$root = "D:" # drive letter root to host scripts/logs etc
+$MSSQLroot = "D:" # drive letter root of MSSQL data directory
 
 $CID = "C01090"
-$DataDir = "D:\AppData\Rundeck\$CID\Data"
-$LogDir = "D:\AppData\Rundeck\$CID\Logs"
+$DataDir = "$root\AppData\Rundeck\$CID\Data"
+$LogDir = "$root\AppData\Rundeck\$CID\Logs"
 $transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
 
 Start-Transcript -Path $transcriptlog -Force -NoClobber -Append
@@ -14,9 +16,9 @@ Write-Host $(Get-Date)
 $ErrorActionPreference="Stop"
 
 #Parameters
-$Path = "D:\Microsoft SQL Server\MSSQL12.SDSSIMS\MSSQL\Backup" # Path where file(s) are located 
+$Path = "$MSSQLroot\Microsoft SQL Server\MSSQL12.SDSSIMS\MSSQL\Backup" # Path where file(s) are located 
 $Days = "14" # Number of days before current date
-$FileNamePatternMatch = "PARS_*.BAK"
+$FileNamePatternMatch = "PARS_*.BAK" # filename pattern to match for deletion
  
 #remove all Files created older than the last n days
 Get-ChildItem -path "$Path\$FileNamePatternMatch" -Recurse -File | Where CreationTime -lt  (Get-Date).AddDays(-$Days) | Remove-Item -verbose
