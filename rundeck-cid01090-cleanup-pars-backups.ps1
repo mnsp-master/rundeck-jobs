6627b32bc60da710f6af5539b35524471cdfa269
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-06-12 09:36:41 +0100

    0936

--- rundeck-cid01090-cleanup-pars-backups.ps1
+++ rundeck-cid01090-cleanup-pars-backups.ps1
@@ -1,8 +1,8 @@
-cls
+Clear-host
 
-$CID="C01090"
-$DataDir="D:\AppData\Rundeck\$CID\Data"
-$LogDir="D:\AppData\Rundeck\$CID\Logs"
+$CID = "C01090"
+$DataDir = "D:\AppData\Rundeck\$CID\Data"
+$LogDir = "D:\AppData\Rundeck\$CID\Logs"
 $transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
 
 Start-Transcript -Path $transcriptlog -Force -NoClobber -Append
@@ -15,26 +15,13 @@ $ErrorActionPreference="Stop"
 
 #Parameters
 $Path = "D:\Microsoft SQL Server\MSSQL12.SDSSIMS\MSSQL\Backup" # Path where the file is located 
-$Days = "30" # Number of days before current date
+$Days = "15" # Number of days before current date
 $FileNamePatternMatch = "PARS_*.BAK"
  
-#Calculate Cutoff date
-#$CutoffDate = (Get-Date).AddDays(-$Days)
- 
-#Get All Files modified more than the last 30 days
-#Get-ChildItem -Path $Path -Recurse -File | Where-Object { $_.LastWriteTime -lt $CutoffDate } #| Remove-Item –Force -Verbose
-Get-ChildItem "$Path\$FileNamePatternMatch" -Recurse -File | Where CreationTime -lt  (Get-Date).AddDays(-30)
+#Get All Files modified more than the last n days
+Get-ChildItem -path "$Path\PARS_*.BAK" -Recurse -File | Where CreationTime -lt  (Get-Date).AddDays(-$Days) | Remove-Item -verbose
+
+#remove all log files older than 30 days
+Get-ChildItem "$logdir\*_transcript.log" -Recurse -File | Where CreationTime -lt  (Get-Date).AddDays(-30) | Remove-Item -verbose
 
 Stop-Transcript
-
-<#
-$WorkDir="D:\AppData\Rundeck"
-$CID="DEV9999"
-$GamDir="D:\AppData\GAMXTD3\app"
-$DataDir="D:\AppData\Rundeck\$CID\Data"
-$LogDir="D:\AppData\Rundeck\$CID\Logs"
-$transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
-
-Get-ChildItem "$Root\$RundeckDir\*_transcript.log" -Recurse -File | Where CreationTime -lt  (Get-Date).AddDays(-30) | Remove-Item -verbose
-
-#>
