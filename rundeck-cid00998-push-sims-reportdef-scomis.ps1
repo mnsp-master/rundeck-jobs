3ced2db1fa01374e6c381d616c79dd2d2825b6d1
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-07-04 14:32:33 +0100

    14:32

--- rundeck-cid00998-push-sims-reportdef-scomis.ps1
+++ rundeck-cid00998-push-sims-reportdef-scomis.ps1
@@ -27,7 +27,7 @@ Write-Host "Downloading Sims Report Definitions to distribute to all sims instan
 
     Set-Location $GamDir
     #Invoke-Expression "$GamDir\gam.exe user $GoogleGamMail get drivefile id $SimsReportGoogleDocID targetname '$SimsReport'" -ErrorAction SilentlyContinue
-    Write-host "$GamDir\gam.exe user $GoogleGamMail print filelist select $SimsReportDefsGoogleFolderID fields id,name > $tempcsv" #-ErrorAction SilentlyContinue
+    Invoke-Expression "$GamDir\gam.exe user $GoogleGamMail print filelist select $SimsReportDefsGoogleFolderID fields id,name" #-ErrorAction SilentlyContinue
     #gam user snoble@writhlington.org.uk print filelist select 1Mw7sWJIdXAGJLBbbOV0CXlX-NibOX4o5 fields id,name > f:\tmp\test.csv
 
 Write-Host "loop through each sims instance..."
