fab7f918b50d6f15ac089f482f48a9fc04a55e4a
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-08-01 11:11:02 +0100

    11:10

--- rundeck-cid01161-mass-create-changes.ps1
+++ rundeck-cid01161-mass-create-changes.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.0.0.0.8"
+$mnspver = "0.0.0.0.0.9"
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
 Start-Sleep 10
@@ -11,22 +11,48 @@ foreach ($TargetEntityID in $TargetEntityIDs) {
         Write-Host "Target Entity ID:" $TargetEntityID
 }
 
-#<#
+
 #create api session to glpi instance...
 $SessionToken = Invoke-RestMethod -Verbose "$AppURL/initSession" -Method Get -Headers @{"Content-Type" = "application/json";"Authorization" = "user_token $UserToken";"App-Token"=$AppToken}
 #https://www.urldecoder.org/
 
-Write-Host "get some info from GLPI..."
-$EntityResult = @() #empty array
-$EntityResult = Invoke-RestMethod "$AppURL/Entity/1" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
-
-$EntityResult
 
 #$EntityResult = Invoke-RestMethod "$AppURL/search/Entity?is_deleted=0&as_map=0&range=0-1000000&criteria[0][link]=AND&criteria[0][field]=1&criteria[0][searchtype]=notequals&criteria[0][value]=0&search=Search&itemtype=Entity&start=0" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
 
 #$entities = $EntityResult.data #convert api search into entities array
 #$entities
 
+
+Write-Host "Creating Change..."
+
+$data = @{
+    "input" = @(
+        @{
+            "content" = "Change description $(Get-Date)"
+            "name" = "API created $(Get-Date)"
+            "_users_id_requester" = "47"
+            "entities_id" = "1"
+            "priority" = "3"
+            "urgency" = "2"
+            "status" = "1"
+            "impact" = "3"
+        }
+    )
+}
+
+
+$json = $data | ConvertTo-Json
+Invoke-RestMethod -Method POST -Uri $ChangeCreateUrl -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"} -Body $json -ContentType 'application/json'
+
 #close current api session...
 Invoke-RestMethod "$AppURL/killSession" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
+
+
+<#
+Write-Host "get some info from GLPI..."
+$EntityResult = @() #empty array
+$EntityResult = Invoke-RestMethod "$AppURL/Entity/1" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
+
+$EntityResult
+
 #>
