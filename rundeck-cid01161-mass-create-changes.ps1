157632ce4f1563c522c90f86436f66a1e620ae2f
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-08-01 09:41:52 +0100

    09:41

--- rundeck-cid01161-mass-create-changes.ps1
+++ rundeck-cid01161-mass-create-changes.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.0.0.0.5"
+$mnspver = "0.0.0.0.0.6"
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
 Start-Sleep 10
@@ -11,11 +11,18 @@ foreach ($TargetEntityID in $TargetEntityIDs) {
         Write-Host "Target Entity ID:" $TargetEntityID
 }
 
-<#
+#<#
 #create api session to glpi instance...
 $SessionToken = Invoke-RestMethod -Verbose "$AppURL/initSession" -Method Get -Headers @{"Content-Type" = "application/json";"Authorization" = "user_token $UserToken";"App-Token"=$AppToken}
 #https://www.urldecoder.org/
 
+#get all entities from GLPI
+$EntityResult = @() #empty array
+
+$EntityResult = Invoke-RestMethod "$AppURL/search/Entity?is_deleted=0&as_map=0&range=0-1000000&criteria[0][link]=AND&criteria[0][field]=1&criteria[0][searchtype]=notequals&criteria[0][value]=0&search=Search&itemtype=Entity&start=0" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
+
+$entities = $EntityResult.data #convert api search into entities array
+$entities
 
 #close current api session...
 Invoke-RestMethod "$AppURL/killSession" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
