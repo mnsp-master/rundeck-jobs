8de8f0531e21899767016f1b1f43ce037bb7ea55
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-11-29 12:59:32 +0000

    12:59

--- rundeck-cid01161-mass-create-changes.ps1
+++ rundeck-cid01161-mass-create-changes.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.0.0.0.2.9.6"
+$mnspver = "0.0.0.0.0.2.9.7"
 $TicketCreateUrl = "$AppURL/Ticket"
 $ChangeCreateUrl = "$AppURL/Change"
 $SetActiveEntity = "$AppURL/changeActiveEntities"
@@ -64,10 +64,13 @@ foreach ($TargetEntityID in $TargetEntityIDs) {
 
         Write-Host "Creating Change for entity ID:" $TargetEntityID
         #Write-Host "Administrative Number: " $GetEntityAttributes.registration_number
-        Write-Host "SchoolNameCode: " $GetEntityAttributes.$GLPIsearchStringSchoolNameCodeID
+        #Write-Host "SchoolNameCode: " $GetEntityAttributes.$GLPIsearchStringSchoolNameCodeID
 
-        #$dataName = "$($EntityResult.data.76694) - $ItemTitle $(Get-Date)"
+        $dataName = "($($EntityResult.data.76694) - $ItemTitle $(Get-Date))"
         $dataUsersIdAssign = $($EntityResult.data.76692)
+
+        Write-Host "SNO 01..."
+        $dataName
 
 
         $data = @{
