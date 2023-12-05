9f0364d61e1856fd89cd8adacfd4ba3e532678e1
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-12-05 12:30:34 +0000

    12:30

--- rundeck-cid01161-mass-create-changes.ps1
+++ rundeck-cid01161-mass-create-changes.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.0.0.0.3.2.8"
+$mnspver = "0.0.0.0.0.3.2.9"
 $TicketCreateUrl = "$AppURL/Ticket"
 $ChangeCreateUrl = "$AppURL/Change"
 $SetActiveEntity = "$AppURL/changeActiveEntities"
@@ -10,9 +10,7 @@ Write-Host "MNSP Version" $mnspver
 Start-Sleep 10
 $ErrorActionPreference="Continue"
 
-#
-
-$TargetEntityIDs = $($TargetEntityIDs.split(','))
+$TargetEntityIDs = $($TargetEntityIDs.split(',')) #split supplied value using commas
 
 
 #create api session to glpi instance...
@@ -20,17 +18,11 @@ $SessionToken = Invoke-RestMethod -Verbose "$AppURL/initSession" -Method Get -He
 #https://www.urldecoder.org/
 
 
-#$EntityResult = Invoke-RestMethod "$AppURL/search/Entity?is_deleted=0&as_map=0&range=0-1000000&criteria[0][link]=AND&criteria[0][field]=1&criteria[0][searchtype]=notequals&criteria[0][value]=0&search=Search&itemtype=Entity&start=0" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
-
-#$entities = $EntityResult.data #convert api search into entities array
-#$entities
-
 ################################ return GLPI plugin additional fields IDs #######################################
 $ApiSearchResult = Invoke-RestMethod "$AppURL/listSearchOptions/Entity" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"} # api serach query for glpi entities
-$ApiSearchResult  | out-file -FilePath $temptxt # output api entity query to tmp txt doc
+$ApiSearchResult  | out-file -FilePath $temptxt # output api entity query to tmp txt file
 $ApiSearchResultSummary = Get-Content $temptxt | where-object {$_ -Like "*MNSP IT Adhoc*"} | Select-Object #filter to only include specific Plugin generated ID's
 
-#$ApiSearchResultSummary
 #get specific GLPI plugin additional fields object IDs...
 $GLPIsearchStringSchoolType = "MNSP IT Adhoc - School Type"
 $MNSPSchoolTypeID = $($ApiSearchResultSummary | where-Object {$_ -Like "*$GLPIsearchStringSchoolType*"}).split(":")[0].TrimEnd()
@@ -43,15 +35,6 @@ Write-Host "$GLPIsearchStringLevel3ITEngineer ID: ---$MNSPLevel3EngineerID---"
 $GLPIsearchStringSchoolNameCode = "MNSP IT Adhoc - School Name Code"
 $MNSPSchoolNameCodeID = $($ApiSearchResultSummary | where-Object {$_ -Like "*$GLPIsearchStringSchoolNameCode*"}).split(":")[0].TrimEnd()
 Write-Host "$GLPIsearchStringSchoolNameCode ID: ---$MNSPSchoolNameCodeID---"
-
-#close current api session...
-#Invoke-RestMethod "$AppURL/killSession" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
-#exit
-
-
-#$GLPIsearchStringGoogleWorkspaceChromebookBaseOU = "MNSP IT Adhoc - Google workspace chrome book base OU" #search string to return Plugin object ID
-#$GoogleWorkspaceChromebookBaseOUID = $($ApiSearchResultSummary | Where-Object {$_ -Like "*$GLPIsearchStringGoogleWorkspaceChromebookBaseOU*"}).split(":")[0].TrimEnd() #get headteacher ID
-#Write-host "$GLPIsearchStringGoogleWorkspaceChromebookBaseOU ID: ---$GoogleWorkspaceChromebookBaseOUID---"
 
 
 #determine if input value is all primaries/secondaries/AP etc....
@@ -66,9 +49,6 @@ if ( $TargetEntityIDs -eq "1000" ) {
     $EntitiesResult = Invoke-RestMethod "$AppURL/search/Entity$apiQueryALL" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
     $TargetEntityIDs = $EntitiesResult.data.2
     $TargetEntityIDs
-    #close current api session...
-    #Invoke-RestMethod "$AppURL/killSession" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
-    #exit
     }
 
 elseif 
@@ -79,8 +59,6 @@ elseif
     $EntitiesResult = Invoke-RestMethod "$AppURL/search/Entity$apiQueryALL" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
     $TargetEntityIDs = $EntitiesResult.data.2
     $TargetEntityIDs
-    #Invoke-RestMethod "$AppURL/killSession" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
-    #exit
     }
 
 elseif
@@ -91,8 +69,6 @@ elseif
     $EntitiesResult = Invoke-RestMethod "$AppURL/search/Entity$apiQueryALL" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
     $TargetEntityIDs = $EntitiesResult.data.2
     $TargetEntityIDs
-    #Invoke-RestMethod "$AppURL/killSession" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
-    #exit
     }
 
 
@@ -100,10 +76,6 @@ foreach ($TargetEntityID in $TargetEntityIDs) {
         Write-Host "Getting Entity Info..."
         $GetEntityAttributes = @()
         $GetEntityAttributes = Invoke-RestMethod -Method GET -Uri $EntityAttributesURL/$TargetEntityID -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken" ; "ContentType" = "application/json"}
-        #$GetEntityAttributes
-
-        #get additional fields plugin values
-        #$apiQuerySpecificID = "?as_map=0&browse=0&criteria[0][link]=AND&criteria[0][field]=76694&criteria[0][searchtype]=contains&criteria[0][value]=&criteria[1][link]=OR&criteria[1][field]=76692&criteria[1][searchtype]=contains&criteria[1][value]=&criteria[2][link]=OR&criteria[2][field]=76684&criteria[2][searchtype]=contains&criteria[2][value]=&criteria[3][link]=AND&criteria[3][field]=2&criteria[3][searchtype]=contains&criteria[3][value]=$TargetEntityID&itemtype=Entity&start=0"
 
         $apiQuerySpecificID = "?as_map=0&browse=0&criteria[0][link]=AND&criteria[0][field]=$MNSPSchoolNameCodeID&criteria[0][searchtype]=contains&criteria[0][value]=&criteria[1][link]=OR&criteria[1][field]=$MNSPLevel3EngineerID&criteria[1][searchtype]=contains&criteria[1][value]=&criteria[2][link]=OR&criteria[2][field]=$MNSPSchoolTypeID&criteria[2][searchtype]=contains&criteria[2][value]=&criteria[3][link]=AND&criteria[3][field]=2&criteria[3][searchtype]=contains&criteria[3][value]=$TargetEntityID&itemtype=Entity&start=0"
 
@@ -112,21 +84,12 @@ foreach ($TargetEntityID in $TargetEntityIDs) {
         $EntityResult.data
 
         Write-Host "Creating Change for entity ID:" $TargetEntityID
-        #Write-Host "Administrative Number: " $GetEntityAttributes.registration_number
-        #Write-Host "SchoolNameCode: " $GetEntityAttributes.$GLPIsearchStringSchoolNameCodeID
 
         $dataName = @()
-        #$dataName = "$($EntityResult.data.76694) - $ItemTitle" # $(Get-Date)
         $dataName = "$($EntityResult.data.$MNSPSchoolNameCodeID) - $ItemTitle" # $(Get-Date)
-        #$dataName = "$($EntityResult.data.76684) - $ItemTitle" # $(Get-Date)
         $($EntityResult.data.$MNSPSchoolNameCodeID)
-        #$dataUsersIdAssign = $($EntityResult.data.76692)
-        #$dataUsersIdAssign = $($EntityResult.data.76683)
         $dataUsersIdAssign = $($EntityResult.data.$MNSPLevel3EngineerID)
         $($EntityResult.data.$MNSPLevel3EngineerID)
-
-        #Write-Host "SNO 01..."
-        #$dataName
 
 
         $data = @{
@@ -153,8 +116,7 @@ foreach ($TargetEntityID in $TargetEntityIDs) {
     $CreatedChangeID = $($ApiAction.id)
     Write-Host "Created Change ID: " $CreatedChangeID
 
-    #Link Change with Project ID:
-
+    #Link Change(s) with GLPI Project ID:
         $ProjectData = @{
         "input" = @(
             @{
@@ -167,7 +129,7 @@ foreach ($TargetEntityID in $TargetEntityIDs) {
     $ProjectDataJson = $ProjectData | ConvertTo-Json
     Invoke-RestMethod -Method POST -Uri $ProjectUpdateUrl/$LinkedProjectID/Itil_Project -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"} -Body $ProjectDataJson -ContentType 'application/json'
 
-    ##create project task##
+    ##create project task(s)##
     $ProjectUpdateUrl = "$AppURL/Project"
         $ProjectData = @{
             "input" = @(
@@ -191,7 +153,24 @@ foreach ($TargetEntityID in $TargetEntityIDs) {
 Invoke-RestMethod "$AppURL/killSession" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
 
 <#
+DEV NOTES/Snips...
 
+#$ApiSearchResultSummary
+
+#close current api session...
+#Invoke-RestMethod "$AppURL/killSession" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
+#exit
+
+
+#$GLPIsearchStringGoogleWorkspaceChromebookBaseOU = "MNSP IT Adhoc - Google workspace chrome book base OU" #search string to return Plugin object ID
+#$GoogleWorkspaceChromebookBaseOUID = $($ApiSearchResultSummary | Where-Object {$_ -Like "*$GLPIsearchStringGoogleWorkspaceChromebookBaseOU*"}).split(":")[0].TrimEnd() #get headteacher ID
+#Write-host "$GLPIsearchStringGoogleWorkspaceChromebookBaseOU ID: ---$GoogleWorkspaceChromebookBaseOUID---"
+
+
+#$EntityResult = Invoke-RestMethod "$AppURL/search/Entity?is_deleted=0&as_map=0&range=0-1000000&criteria[0][link]=AND&criteria[0][field]=1&criteria[0][searchtype]=notequals&criteria[0][value]=0&search=Search&itemtype=Entity&start=0" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
+
+#$entities = $EntityResult.data #convert api search into entities array
+#$entities
 
 $GLPIsearchStringSchoolNameCodeID = $($ApiSearchResultSummary | Where-Object {$_ -Like "*$GLPIsearchStringSchoolNameCode*"}).split(":")[0].TrimEnd() #get SchoolNameCode ID
 Write-host "$GLPIsearchStringSchoolNameCode ID: ---$GLPIsearchStringSchoolNameCodeID---"
