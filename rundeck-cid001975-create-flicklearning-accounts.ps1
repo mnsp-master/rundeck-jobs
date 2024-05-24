69a4725ef902b737f615d76abb6fed3270027920
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-05-24 11:27:06 +0100

    11:27

--- rundeck-cid001975-create-flicklearning-accounts.ps1
+++ rundeck-cid001975-create-flicklearning-accounts.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.5"
+$mnspver = "0.0.6"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
@@ -9,6 +9,22 @@ Set-Location $GamDir
 #Get/Confirm Google instance
 Invoke-Expression "$GamDir\gam.exe info domain" 
 
+#download gsheet
+
+Write-Host "Downloading Googlesheet containing all required users..."
+$SrcUserDataCSV = $tempcsv
+    if (Test-Path -path $SrcUserDataCSV ) {
+
+        Write-Host "$SrcUserDataCSV exists, deleting..."
+        Remove-Item -Path $SrcUserDataCSV -Force
+    }
+
+    Set-Location $GamDir
+
+    Invoke-Expression "$GamDir\gam.exe user $GoogleGamMail get drivefile id $GsheetUserSourceID format csv targetfolder $datadir" -ErrorAction SilentlyContinue
+Start-Sleep 2
+$userSource = Import-Csv -Path $SrcUserDataCSV
+
 
 $AppParams = -join ("&users[0][email]=",$email,"&users[0][firstname]=",$FirstName,"&users[0][lastname]=",$LastName,"&users[0][password]=",$Password)
 $AppFullURL = -join ($AppURL,$AppFunction,$APIToken,$AppParams) #create full rest api url
@@ -17,9 +33,10 @@ Write-Host "Full App URL..."
 $AppFullURL
 
 #create user
-$userResult = Invoke-RestMethod $AppFullURL #compose restapi
-$userResult 
+#$userResult = Invoke-RestMethod $AppFullURL #compose restapi
+#$userResult 
 
-Write-Host "User creation response..."
-$userResult.RESPONSE.MULTIPLE.SINGLE.KEY #create user response
+#Write-Host "User creation response..."
+#$userResult.RESPONSE.MULTIPLE.SINGLE.KEY #create user response
+
 
