2fbeaabfb51560d9960c5622dbd64c19fced09a6
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-05-24 11:00:16 +0100

    11:00

--- rundeck-cid001975-create-flicklearning-accounts.ps1
+++ rundeck-cid001975-create-flicklearning-accounts.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.3"
+$mnspver = "0.0.5"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
@@ -13,10 +13,14 @@ Invoke-Expression "$GamDir\gam.exe info domain"
 $AppParams = -join ("&users[0][email]=",$email,"&users[0][firstname]=",$FirstName,"&users[0][lastname]=",$LastName,"&users[0][password]=",$Password)
 $AppFullURL = -join ($AppURL,$AppFunction,$APIToken,$AppParams) #create full rest api url
 
+Write-Host "Full App URL..."
 $AppFullURL
 
-$userResult = Invoke-RestMethod $AppFullURL #compose restapi instruction
-$userResult #create user
+#create user
+$userResult = Invoke-RestMethod $AppFullURL #compose restapi
+$userResult 
+
+Write-Host "User creation response..."
 $userResult.RESPONSE.MULTIPLE.SINGLE.KEY #create user response
 
 
