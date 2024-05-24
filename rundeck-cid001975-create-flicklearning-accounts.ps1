0af7d532d8f080f8857faa990a0abda7ffd0d9ff
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-05-24 11:01:37 +0100

    11:01

--- rundeck-cid001975-create-flicklearning-accounts.ps1
+++ rundeck-cid001975-create-flicklearning-accounts.ps1
@@ -23,10 +23,3 @@ $userResult
 Write-Host "User creation response..."
 $userResult.RESPONSE.MULTIPLE.SINGLE.KEY #create user response
 
-
-#All entities: - Production
-#$EntityResult = Invoke-RestMethod "$AppURL/search/Entity?is_deleted=0&as_map=0&range=0-1000000&criteria[0][link]=AND&criteria[0][field]=1&criteria[0][searchtype]=notequals&criteria[0][value]=0&search=Search&itemtype=Entity&start=0" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
-
-#$entities = $EntityResult.data #convert api search into entities array
-#$SearchResult=@()
-#type 14 only
