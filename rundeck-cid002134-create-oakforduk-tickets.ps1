04a435bb6f89a8cc94637b7b8065a38b5a48c4fd
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-07-16 13:32:33 +0100

    13:32

---
+++ rundeck-cid002134-create-oakforduk-tickets.ps1
@@ -0,0 +1,9 @@
+$mnspver = "0.0.1"
+Write-Host $(Get-Date)
+Write-Host "MNSP Version" $mnspver
+
+$ErrorActionPreference="Continue"
+Set-Location $GamDir
+
+#Get/Confirm Google instance
+Invoke-Expression "$GamDir\gam.exe info domain" 
