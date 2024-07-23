8a18a04bdbd3c97aca04703399da8c1e2a1c5563
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-07-23 13:45:41 +0100

    13:45

--- rundeck-cid001975-create-flicklearning-accounts.ps1
+++ rundeck-cid001975-create-flicklearning-accounts.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.38"
+$mnspver = "0.0.39"
 
 Function GeneratePWD {
 #Write-Host $i
@@ -107,8 +107,8 @@ foreach ($SrcUser in $userSource) {
 
 #summary of any skipped users
 $SkippedUserSource = Import-Csv -Path $SrcUserDataCSV | where-object {$_ -match $NoMailCheck}
-Write-Host "Skipped users - no mail attribute present - count: $SkippedUserSource"
-$SkippedUserSource
+Write-Host "Skipped users - no mail attribute present - count: $SkippedUserSource.count"
+$SkippedUserSource | format-table
 
 
 
