b44c22d6048444719bb4bea39ee241303ec73647
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-08-06 14:29:11 +0100

    14:29

--- rundeck-cid002134-create-oakforduk-tickets.ps1
+++ rundeck-cid002134-create-oakforduk-tickets.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.7"
+$mnspver = "0.0.8"
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
 
@@ -11,5 +11,5 @@ Invoke-Expression "$GamDir\gam.exe info domain"
 $Query = "'subject:$GlpiTicketID $GLPITicketSubject'"
 
 # get message id from search criteria ...
-Invoke-Expression "$GamDir\gam.exe user $GLPIGmailAddress print messages query $Query" > $tempcsv
+Invoke-Expression "$GamDir\gam.exe user $GLPIGmailAddress print messages query $Query" | out-file $tempcsv
 
