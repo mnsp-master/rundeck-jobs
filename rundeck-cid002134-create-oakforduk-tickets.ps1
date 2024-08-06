cf94f0e576ea8d099545b50f1facdee6e777baa2
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-08-06 15:23:31 +0100

    15:23

--- rundeck-cid002134-create-oakforduk-tickets.ps1
+++ rundeck-cid002134-create-oakforduk-tickets.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.12"
+$mnspver = "0.0.13"
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
 
@@ -12,7 +12,24 @@ $Query = "'subject:$GlpiTicketID $GLPITicketSubject'"
 #$Query = "'subject:$GlpiTicketID'"
 
 # get message id from search criteria ...
-Write-Host "$GamDir\gam.exe user $GLPIGmailAddress print messages query $Query"
+#Write-Host "$GamDir\gam.exe user $GLPIGmailAddress print messages query $Query"
 Invoke-Expression "$GamDir\gam.exe user $GLPIGmailAddress print messages query $Query"
 Invoke-Expression "$GamDir\gam.exe user $GLPIGmailAddress print messages query $Query" | out-file $tempcsv
+
+Write-host "importing csv data..."
+$GmailMessage = import-csv $tempcsv
+
+Write-Host "Getting mail domain from user..."
+$SenderDomain = $GmailMessage.user.Split("@")[1]
+$SenderDomain
+
+Write-host "removing < and  > from message ID..."
+$MessageID = $GmailMessage.'Message-ID'.TrimStart("<")
+$MessageID = $MessageID.TrimEnd(">")
+$MessageID
+
+$MailReceiver = "$ReceiverPrefix@$senderDomain"
+$MailReceiver
+
+Write-host "$GamDir\gam.exe user $GLPIGmailAddress forward threads to $MailReceiver query "rfc822msgid:$MessageID" subject "Test ALternate subject"
 
