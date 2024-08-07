8ebf4f4cac304ff5e31e092095c63d4655b620cd
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-08-07 13:18:43 +0100

    13:18

--- rundeck-cid002134-create-oakforduk-tickets.ps1
+++ rundeck-cid002134-create-oakforduk-tickets.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.51"
+$mnspver = "0.0.52"
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
 
@@ -27,7 +27,7 @@ $MessageID = $MessageID.TrimEnd(">")
 Write-host "removing < and > from message ID - trimmed ID:" $MessageID
 
 #set message receiver by splitting requester and domain
-### DEV ### $MailReceiver = "$ReceiverPrefix@$senderDomain"
+$MailReceiver = "$ReceiverPrefix@$senderDomain"
 Write-Host "Setting message receiver to:" $MailReceiver
 
 $RFCQuery = "'rfc822msgid:$MessageID'" #concatenate message query
