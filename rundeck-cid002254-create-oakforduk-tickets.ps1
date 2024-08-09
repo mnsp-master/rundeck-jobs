7a6e48a970a40dc74c729986c614cf7e7fda8919
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-08-09 13:06:33 +0100

    13:06

--- rundeck-cid002254-create-oakforduk-tickets.ps1
+++ rundeck-cid002254-create-oakforduk-tickets.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.6"
+$mnspver = "0.0.7"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
@@ -10,10 +10,53 @@ Set-Location $GamDir
 $SessionToken = Invoke-RestMethod -Verbose "$AppURL/initSession" -Method Get -Headers @{"Content-Type" = "application/json";"Authorization" = "user_token $UserToken";"App-Token"=$AppToken}
 #https://www.urldecoder.org/
 
+
+#######
+$previouslyProcessedbyID = @()
+$previouslyProcessedbyID = import-csv $tempcsv1
+#$previouslyProcessedbyID
+
 # all tickets
 $TicketResult = @()
 $TicketResult = Invoke-RestMethod "$AppURL/search/Ticket?is_deleted=0&as_map=0&browse=0&criteria[0][link]=AND&criteria[0][field]=23&criteria[0][searchtype]=equals&criteria[0][value]=12&itemtype=Ticket&start=0" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
-$TicketResult.data
+$TicketData = $TicketResult.data
+
+Write-Host "Ticket id's found that match search criteria:"
+$TicketData.2 #ticket id's only
+Write-Host "-----------------------------------------------`n"
+
+#$TicketData = @()
+foreach ($Ticket in $TicketData){
+
+$TicketID = @()
+$TicketID = $Ticket.2
+Write-Host "Assessing Ticket ID:" $TicketID
+#check if ticket has been previously processed...
+Write-Host "ticket number check:" $previouslyProcessedbyID.GLPITicketID
+if ($previouslyProcessedbyID -Match $TicketID) {
+    Write-Host "ID: $($TicketID) is a Previously processed Ticket..."
+    Write-Host "-----------------------------------------------`n"
+    } else {
+    Write-Host "ID: $($TicketID) will be processed..."
+    Write-Host "GLPI User ID: $($Ticket.4)"
+    #get user ID: Details
+    $userDetail = @()
+    $userDetail = Invoke-RestMethod "$AppURL/search/User?is_deleted=0&as_map=0&browse=0&criteria%5B0%5D%5Blink%5D=AND&criteria%5B0%5D%5Bfield%5D=2&criteria%5B0%5D%5Bsearchtype%5D=contains&criteria%5B0%5D%5Bvalue%5D=$($Ticket.4)&itemtype=User&start=0" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
+    $Mail = @()
+    $Mail = $userDetail.data.5
+    Write-Host "Users email address:" $Mail
+    #process mail using gamxtd
+
+    #add-content -path $tempcsv1 -Value $TicketID
+    #$TicketID | export-csv -append -path $tempcsv1
+    $TicketID | out-file -Append $tempcsv1 #csv must be encoded as UCS-2 LE BOM
+    start-sleep 1
+    Write-Host "-----------------------------------------------`n"
+    
+    }
+}
+
+
 
 
 Start-Sleep 10
@@ -22,6 +65,12 @@ Start-Sleep 10
 Invoke-RestMethod "$AppURL/killSession" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
 
 <#
+
+# all tickets
+$TicketResult = @()
+$TicketResult = Invoke-RestMethod "$AppURL/search/Ticket?is_deleted=0&as_map=0&browse=0&criteria[0][link]=AND&criteria[0][field]=23&criteria[0][searchtype]=equals&criteria[0][value]=12&itemtype=Ticket&start=0" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
+$TicketResult.data
+
 #All entities: - Production
 $EntityResult = Invoke-RestMethod "$AppURL/search/Entity?is_deleted=0&as_map=0&range=0-1000000&criteria[0][link]=AND&criteria[0][field]=1&criteria[0][searchtype]=notequals&criteria[0][value]=0&search=Search&itemtype=Entity&start=0" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
 
