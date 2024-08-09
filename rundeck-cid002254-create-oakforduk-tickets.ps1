1074ad80efdcef0b13eacb184464c3e599a5707a
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-08-09 08:42:31 +0100

    08:42

--- rundeck-cid002254-create-oakforduk-tickets.ps1
+++ rundeck-cid002254-create-oakforduk-tickets.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.3"
+$mnspver = "0.0.4"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
@@ -11,8 +11,8 @@ $SessionToken = Invoke-RestMethod -Verbose "$AppURL/initSession" -Method Get -He
 #https://www.urldecoder.org/
 
 # all tickets
-#$TicketResult = Invoke-RestMethod "$AppURL/search/ticket?is_deleted=0&as_map=0&browse=0&criteria[0][link]=AND&criteria[0][field]=23&criteria[0][searchtype]=equals&criteria[0][value]=12&itemtype=Ticket&start=0"
-#$TicketResult
+$TicketResult = Invoke-RestMethod "$AppURL/search/ticket?is_deleted=0&as_map=0&browse=0&criteria[0][link]=AND&criteria[0][field]=23&criteria[0][searchtype]=equals&criteria[0][value]=12&itemtype=Ticket&start=0" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
+$TicketResult
 
 
 Start-Sleep 10
