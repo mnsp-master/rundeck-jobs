4c1a9bc4895aef255eaa562fe46c4fe554c12a11
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-09-02 21:35:29 +0100

    21:35

--- rundeck-cid002271-update-all-year-passwords.ps1
+++ rundeck-cid002271-update-all-year-passwords.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.8"
+$mnspver = "0.0.9"
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
 
@@ -32,7 +32,7 @@ $pwd = $(Invoke-WebRequest -Uri $PwdGenURL -UseBasicParsing)
         $password = "Js653151MH$"
         }
 
-$Fullname = "$user.GivenName $User.Surname"
+$Fullname = "$($user.GivenName) $($User.Surname)"
 $email = "$user@$mailDomain"
 
 Write-Host "Processing User: $Fullname $user $email new password as of $(Get-Date): $password"
