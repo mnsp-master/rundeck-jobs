13b124e9accdd0813c98710016bb409c7d804cf8
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-09-02 16:19:25 +0100

    16:19

--- rundeck-cid002271-update-all-year-passwords.ps1
+++ rundeck-cid002271-update-all-year-passwords.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.2"
+$mnspver = "0.0.3"
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
 
@@ -10,3 +10,11 @@ Clear-Content $tempcsv
 sleep 2
 write-host $csvheader
 $csvheader | out-file -filepath $tempcsv -Append #create blank csv with simple header
+
+$PlainPassword = $ADDomainPass
+$SecurePassword = $PlainPassword | ConvertTo-SecureString -AsPlainText -Force
+$UserName = $ADDomainUser
+
+$users = Get-ADUser -Credential $Credentials -filter * -SearchBase $SearchBase
+
+$users
