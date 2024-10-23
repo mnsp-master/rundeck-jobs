6f74b456e794b0c8d0d45d6192decd8eec752616
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-10-23 16:27:10 +0100

    16:27

--- rundeck-cid02430-staff-migtration-prep.ps1
+++ rundeck-cid02430-staff-migtration-prep.ps1
@@ -1,9 +1,15 @@
-$mnspver = "0.0.1"
+$mnspver = "0.0.2"
 
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
 Start-Sleep 10
 $ErrorActionPreference="Continue"
-#Set-Location $GamDir
+Set-Location $GamDir
+
+Invoke-Expression "$GamDir\gam.exe select $GoogleWorkSpaceSource save" # swap/set google workspace
+Invoke-Expression "$GamDir\gam.exe info domain"
+
+Invoke-Expression "$GamDir\gam.exe select $GoogleWorkSpaceDestination save" # swap/set google workspace
+Invoke-Expression "$GamDir\gam.exe info domain"
 
