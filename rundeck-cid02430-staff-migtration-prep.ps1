489fb6a92cb49ff101ad1e6176e9618aa3fbb1ae
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-10-24 13:12:08 +0100

    13:12

--- rundeck-cid02430-staff-migtration-prep.ps1
+++ rundeck-cid02430-staff-migtration-prep.ps1
@@ -1,5 +1,4 @@
-$mnspver = "0.0.13"
-
+$mnspver = "0.0.14"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
@@ -7,11 +6,14 @@ Start-Sleep 10
 $ErrorActionPreference="Continue"
 Set-Location $GamDir
 
-Write-host "--------------------------------------`n"
+function DashedLine {
+Write-host "-----------------------------------------------------------`n"
+}
 
 Write-Host "Setting workspace source: $GoogleWorkSpaceSource"
 Invoke-Expression "$GamDir\gam.exe select $GoogleWorkSpaceSource save" # swap/set google workspace
-Invoke-Expression "$GamDir\gam.exe info domain"
+Invoke-Expression "$GamDir\gam.exe"
+DashedLine
 
 Write-Host "Getting members of users to process source group $GoogleWorkspaceSourceGroup"
 Invoke-Expression "$GamDir\gam.exe print group-members group_ns $GoogleWorkspaceSourceGroup > $tempcsv"
@@ -19,9 +21,10 @@ Invoke-Expression "$GamDir\gam.exe print group-members group_ns $GoogleWorkspace
 $UsersToProcess = Import-csv $tempcsv
 
 foreach ($user in $UsersToProcess) {
+    DashedLine
     $usermail = $User.email
     Write-Host "Processing $usermail"
-    Write-host "--------------------------------------`n"
+    DashedLine
 }
 
 
