4e169d828882a3e70f46e72f75038ea7e2088ba7
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-10-24 10:27:20 +0100

    10:27

--- rundeck-cid02430-staff-migtration-prep.ps1
+++ rundeck-cid02430-staff-migtration-prep.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.12"
+$mnspver = "0.0.13"
 
 
 Write-Host $(Get-Date)
@@ -16,17 +16,24 @@ Invoke-Expression "$GamDir\gam.exe info domain"
 Write-Host "Getting members of users to process source group $GoogleWorkspaceSourceGroup"
 Invoke-Expression "$GamDir\gam.exe print group-members group_ns $GoogleWorkspaceSourceGroup > $tempcsv"
 
-#<
-Start-sleep 5
-Write-host "--------------------------------------`n"
+$UsersToProcess = Import-csv $tempcsv
+
+foreach ($user in $UsersToProcess) {
+    $usermail = $User.email
+    Write-Host "Processing $usermail"
+    Write-host "--------------------------------------`n"
+}
+
+
+#Start-sleep 5
+#Write-host "--------------------------------------`n"
 
 #Write-Host "Setting workspace destination: $GoogleWorkSpaceDestination"
 #Invoke-Expression "$GamDir\gam.exe select $GoogleWorkSpaceDestination save" # swap/set google workspace
 #Invoke-Expression "$GamDir\gam.exe"
 
-Write-host "--------------------------------------`n"
+#Write-host "--------------------------------------`n"
 
 #Start-sleep 5
-Write-Host $(Get-Date)
-#>
+#Write-Host $(Get-Date)
 
