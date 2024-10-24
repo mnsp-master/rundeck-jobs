d9da07d5f91c55419c3940799678166119e7b5c6
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-10-24 13:58:13 +0100

    13:58

--- rundeck-cid02430-staff-migtration-prep.ps1
+++ rundeck-cid02430-staff-migtration-prep.ps1
@@ -1,10 +1,12 @@
-$mnspver = "0.0.14"
+$mnspver = "0.0.16"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
 Start-Sleep 10
 $ErrorActionPreference="Continue"
 Set-Location $GamDir
+
+$GoogleSvcAccount = $GoogleServiceAccountPrefix$$$GoogleWorkSpaceSource@GoogleWorkspaceDomain
 
 function DashedLine {
 Write-host "-----------------------------------------------------------`n"
@@ -18,12 +20,21 @@ DashedLine
 Write-Host "Getting members of users to process source group $GoogleWorkspaceSourceGroup"
 Invoke-Expression "$GamDir\gam.exe print group-members group_ns $GoogleWorkspaceSourceGroup > $tempcsv"
 
+#get verified user data
+#if exist check & remove $tempcsv4
+Write-Host "Invoke-Expression "$GamDir\gam.exe user $GoogleSvcAccount get drivefile $GoogleSheetID format csv gsheet $GoogleSheetTab targetfolder $DataDir targetname $tempcsv4""
+
+
 $UsersToProcess = Import-csv $tempcsv
 
 foreach ($user in $UsersToProcess) {
     DashedLine
     $usermail = $User.email
     Write-Host "Processing $usermail"
+
+    #set custom attribute
+
+    
     DashedLine
 }
 
