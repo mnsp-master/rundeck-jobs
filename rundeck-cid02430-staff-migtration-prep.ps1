8e95664d15f6cfc904e987b8ca778f52a62b00d9
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-10-25 09:16:08 +0100

    09:16

--- rundeck-cid02430-staff-migtration-prep.ps1
+++ rundeck-cid02430-staff-migtration-prep.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.33"
+$mnspver = "0.0.34"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
@@ -29,9 +29,9 @@ if (test-path $tempcsv4) { remove-item $tempcsv4 -force -verbose }
 Start-sleep 2
 
 #####upto here##### 
-Write-host "Invoke-Expression "$GamDir\gam.exe user $GoogleSourceSvcAccount get drivefile $GoogleSheetID format csv gsheet $GoogleSheetTab01 targetfolder $DataDir targetname $tempcsv4"
+Write-host "Invoke-Expression "$GamDir\gam.exe user $GoogleSourceSvcAccount get drivefile $GoogleSheetID format csv gsheet "$GoogleSheetTab01" targetfolder $DataDir targetname $tempcsv4"
 "
-Invoke-Expression "$GamDir\gam.exe user $GoogleSourceSvcAccount get drivefile $GoogleSheetID format csv gsheet $GoogleSheetTab01 targetfolder $DataDir targetname $tempcsv4"
+Invoke-Expression "$GamDir\gam.exe user $GoogleSourceSvcAccount get drivefile $GoogleSheetID format csv gsheet "$GoogleSheetTab01" targetfolder $DataDir targetname $tempcsv4"
 
 
 $UsersToProcess = @()
