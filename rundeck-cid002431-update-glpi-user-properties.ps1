b5198d771099814a247ac532a2e51b0629c56943
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-10-28 08:51:41 +0000

    initial commit

---
+++ rundeck-cid002431-update-glpi-user-properties.ps1
@@ -0,0 +1,65 @@
+$mnspver = "0.0.1"
+
+Write-Host $(Get-Date)
+Write-Host "MNSP Version" $mnspver
+Start-Sleep 10
+$ErrorActionPreference="Continue"
+Set-Location $GamDir
+
+function DashedLine {
+Write-host "-----------------------------------------------------------`n"
+}
+
+Write-Host "Setting workspace source: $GoogleWorkSpaceSource"
+Invoke-Expression "$GamDir\gam.exe select $GoogleWorkSpaceSource save" # swap/set google workspace
+Invoke-Expression "$GamDir\gam.exe"
+DashedLine
+
+$GoogleSourceSvcAccount = ("$GoogleServiceAccountPrefix" + "$GoogleWorkSpaceSource" + "@" + "$GGoogleWorkspaceSourceMailDomain")
+Write-Host "Google Source Service Account: $GoogleSourceSvcAccount"
+
+$GoogleWorkspaceSourceGroup = ("$GoogleWorkspaceSourceGroupPrefix" + "@" + "$GGoogleWorkspaceSourceMailDomain")
+Write-Host "Getting members of users to process source group $GoogleWorkspaceSourceGroup"
+Invoke-Expression "$GamDir\gam.exe print group-members group_ns $GoogleWorkspaceSourceGroup > $tempcsv"
+
+#get verified user data
+#if exist check & remove $tempcsv4
+if (test-path $tempcsv4) { remove-item $tempcsv4 -force -verbose }
+
+Start-sleep 2
+
+Write-Host "downloading gsheet ID: $GoogleSheetID tab: $GoogleSheetTab01" #needs confirmation of approach
+Invoke-Expression "$GamDir\gam.exe user $GoogleSourceSvcAccount get drivefile $GoogleSheetID format csv gsheet ""$GoogleSheetTab01"" targetfolder $DataDir targetname $tempcsv4"
+
+$VerifiedUserData = Get-Content -path $tempcsv4 | select-object -skip 1 | convertFrom-csv | where { $_.$FieldMatch01 -like $FieldString } #import where field like $FieldMatch01, and skip 1st line
+Write Host "Number of records matching selection criteria:" $VerifiedUserData.count
+
+#if ($uuids.Contains($uuid)) { } # if var is in array
+
+
+$UsersToProcess = @()
+$UsersToProcess = Import-csv $tempcsv
+
+foreach ($user in $UsersToProcess) {
+    DashedLine
+    $usermail = $User.email
+    Write-Host "Processing $usermail"
+
+    #set custom attribute
+    
+    DashedLine
+}
+
+
+#Start-sleep 5
+#Write-host "--------------------------------------`n"
+
+#Write-Host "Setting workspace destination: $GoogleWorkSpaceDestination"
+#Invoke-Expression "$GamDir\gam.exe select $GoogleWorkSpaceDestination save" # swap/set google workspace
+#Invoke-Expression "$GamDir\gam.exe"
+
+#Write-host "--------------------------------------`n"
+
+#Start-sleep 5
+#Write-Host $(Get-Date)
+
