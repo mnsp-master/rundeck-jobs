a04e72da76c2a2997b9af715b2d90a0f82eb0624
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-12-19 11:29:29 +0000

    0.0.2

--- rundeck-cid02766-peoplexd-sftp-gsheet-sync.ps1
+++ rundeck-cid02766-peoplexd-sftp-gsheet-sync.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.1"
+$mnspver = "0.0.2"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
@@ -10,6 +10,18 @@ function DashedLine {
 Write-host "-----------------------------------------------------------`n"
 }
 
+DashedLine
+
+#Set Google Instance: Destination...
+Write-Host "###### Set Google instance: Destination... ######"
+
+$GoogleSvcAccount = $GoogleWorkspaceMNSPsvcAccount
+Write-Host "Google Destination Service Account: $GoogleSvcAccount"
+
+Write-Host "Setting workspace Destination: $GoogleWorkSpaceDestination"
+Invoke-Expression "$GamDir\gam.exe select $GoogleWorkSpaceDestination save" # swap/set google workspace
+Invoke-Expression "$GamDir\gam.exe"
+start-sleep 3
 DashedLine
 
 #get data
@@ -24,15 +36,5 @@ Start-sleep 2
 $gsheetsData = import-csv -path $tempcsv4
 Write Host "Number of records matching selection criteria:" $gsheetsData.count
 
-#Set Google Instance: Destination...
-Write-Host "###### Set Google instance: Destination... ######"
 
-$GoogleSvcAccount = $GoogleWorkspaceMNSPsvcAccount
-Write-Host "Google Destination Service Account: $GoogleSvcAccount"
-
-Write-Host "Setting workspace Destination: $GoogleWorkSpaceDestination"
-Invoke-Expression "$GamDir\gam.exe select $GoogleWorkSpaceDestination save" # swap/set google workspace
-Invoke-Expression "$GamDir\gam.exe"
-start-sleep 3
-DashedLine
 
