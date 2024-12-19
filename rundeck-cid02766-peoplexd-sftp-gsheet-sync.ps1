c0c28a00cc696bc349dfecfc384e7eea2dcdeb5f
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-12-19 11:14:28 +0000

    initial commit

---
+++ rundeck-cid02766-peoplexd-sftp-gsheet-sync.ps1
@@ -0,0 +1,38 @@
+$mnspver = "0.0.1"
+
+Write-Host $(Get-Date)
+Write-Host "MNSP Version" $mnspver
+Start-Sleep 10
+$ErrorActionPreference="Continue"
+Set-Location $GamDir
+
+function DashedLine {
+Write-host "-----------------------------------------------------------`n"
+}
+
+DashedLine
+
+#get data
+#if exist check & remove $tempcsv4
+if (test-path $tempcsv4) { remove-item $tempcsv4 -force -verbose }
+
+Write-Host "downloading gsheet ID: $GoogleSheetID"
+Invoke-Expression "$GamDir\gam.exe user $GoogleSourceSvcAccount get drivefile $GoogleSheetID format csv gsheet targetfolder $DataDir targetname $tempcsv4"
+
+Start-sleep 2
+
+$gsheetsData = import-csv -path $tempcsv4
+Write Host "Number of records matching selection criteria:" $gsheetsData.count
+
+#Set Google Instance: Destination...
+Write-Host "###### Set Google instance: Destination... ######"
+
+$GoogleSvcAccount = $GoogleWorkspaceMNSPsvcAccount
+Write-Host "Google Destination Service Account: $GoogleSvcAccount"
+
+Write-Host "Setting workspace Destination: $GoogleWorkSpaceDestination"
+Invoke-Expression "$GamDir\gam.exe select $GoogleWorkSpaceDestination save" # swap/set google workspace
+Invoke-Expression "$GamDir\gam.exe"
+start-sleep 3
+DashedLine
+
