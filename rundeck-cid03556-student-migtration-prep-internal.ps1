f469fb6125fc20810b0a063aeb7b6d886a979b5a
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2025-05-29 11:03:13 +0100

    0.0.36

--- rundeck-cid03556-student-migtration-prep-internal.ps1
+++ rundeck-cid03556-student-migtration-prep-internal.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.35"
+$mnspver = "0.0.36"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
@@ -159,7 +159,7 @@ foreach ($user in $VerifiedUserData) {
                         }
 
                 start-sleep 1
-
+            
             Write-Warning "Creating desired target mail domain email address..."
             Write-Host "Invoke-Expression $GamDir\gam.exe create user $ReplacementUserMail firstname '$FirstName' lastname '$LastName' org '$GoogleWorkspaceDestinationUserOU/$UpdatedDestOU' $GoogleCustomAttribute01 $UPN password $password gal false"
 
