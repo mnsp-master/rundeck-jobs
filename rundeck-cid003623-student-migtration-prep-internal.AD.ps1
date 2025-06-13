ddb27272cf8019a250977f12595d3d9941c385cc
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2025-06-13 13:42:36 +0100

    0.0.20

--- rundeck-cid003623-student-migtration-prep-internal.AD.ps1
+++ rundeck-cid003623-student-migtration-prep-internal.AD.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.19"
+$mnspver = "0.0.20"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
@@ -48,7 +48,7 @@ Start-sleep 2
 Write-Host "Field match:  " $FieldMatch01
 Write-Host "Field String: " $FieldString
 #$VerifiedUserData = Get-Content -path $tempcsv4 | where { $_.$FieldMatch01 -like $FieldString }
-$VerifiedUserData = Get-Content -path $tempcsv4 | convertFrom-csv | where { $_.$FieldMatch01 -like '$FieldString' } #import where field like $FieldMatch01
+$VerifiedUserData = Get-Content -path $tempcsv4 | convertFrom-csv | where { $_.$FieldMatch01 -like $FieldString } #import where field like $FieldMatch01
 
 #$VerifiedUserData = Get-Content -path $tempcsv4 | convertFrom-csv | where-object { 
 #    $_.$FieldMatch01 -like $FieldString -and 
@@ -59,7 +59,7 @@ $VerifiedUserData = Get-Content -path $tempcsv4 | convertFrom-csv | where { $_.$
 
 
 #$VerifiedUserData = Get-Content -path $tempcsv4 | select-object -skip 1 | convertFrom-csv | where { $_.$FieldMatch01 -like $FieldString } #import where field like $FieldMatch01, and skip 1st line
-Write Host "Number of records matching selection criteria:" $VerifiedUserData.count
+Write-Host "Number of records matching selection criteria:" $VerifiedUserData.count
 #TODO - if count 0 break out of script...
 
 #create user info destination gsheet
