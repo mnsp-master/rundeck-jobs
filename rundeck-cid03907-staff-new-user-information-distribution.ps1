afc6445d425cb5633dd5bd655fbdfe6652b064f8
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2025-08-29 08:08:21 +0100

    0.0.2

--- rundeck-cid03907-staff-new-user-information-distribution.ps1
+++ rundeck-cid03907-staff-new-user-information-distribution.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.1"
+$mnspver = "0.0.2"
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
@@ -29,58 +29,39 @@ DashedLine
 if (test-path $tempcsv4) { remove-item $tempcsv4 -force -verbose }
 
 Write-Host "downloading gsheet ID: $GoogleSheetID tab: $GoogleSheetTab01"
-Invoke-Expression "$GamDir\gam.exe user $GoogleSvcAccount get drivefile $GoogleSheetID format csv gsheet ""$GoogleSheetTab01"" targetfolder $DataDir targetname $tempcsv4"
+Invoke-Expression "$GamDir\gam.exe user $GoogleSvcAccount get drivefile $GoogleSheetID format csv targetfolder $DataDir targetname $tempcsv4"
 #Write-Host "Invoke-Expression $GamDir\gam.exe user $GoogleSourceSvcAccount get drivefile $GoogleSheetID format csv gsheet ""$GoogleSheetTab01"" targetfolder $DataDir targetname $tempcsv4"
 
 Start-sleep 2
 
-$VerifiedUserData = Get-Content -path $tempcsv4 | select-object -skip 1 | convertFrom-csv | where { $_.$FieldMatch01 -like $FieldString } #import where field like $FieldMatch01, and skip 1st line
+#$VerifiedUserData = Get-Content -path $tempcsv4 | select-object -skip 1 | convertFrom-csv | where { $_.$FieldMatch01 -like $FieldString } #import where field like $FieldMatch01, and skip 1st line
+$VerifiedUserData = import-csv -path $tempcsv4
 Write-Host "Number of records matching selection criteria:" $VerifiedUserData.count
 #TODO - if count 0 break out of script...
 #$VerifiedUserData
 
-Start-Sleep 10
-
+Start-Sleep 2
 
 Write-Host "Processing users in destination..."
 foreach ($user in $VerifiedUserData) {
     DashedLine
-    $LegacyUserMail = $user."Existing Email Address" #current mail address
-    $HRid = $user."Person ID" # HR id
-    $FirstName = $user."Staff first name" #prefered firstname
-    $lastname = $user."Staff Surname"
-    $ReplacementUserMail = $user."new email"
+    $LegacyUserMail = $user."legacy email" #current mail address
+    $HRid = $user."HR ID" # HR id
+    $FirstName = $user."First Name" #prefered firstname
+    $lastname = $user."Last Name"
+    $ReplacementUserMail = $user."email"
+    $Password = $User."password"
 
     Write-Host "Processing: $LegacyUserMail"
     Write-Host "HR ID: $HRid"
     Write-Host "Firstname: $FirstName"
     Write-Host "Lastname: $lastname"
 
-    #password generator...
-        $pwd = @()
-        try {
-        $pwd = $(Invoke-WebRequest -Uri $PwdWebRequestURI -UseBasicParsing)
-        $Password = $($pwd.content)
-        }
-            catch {
-            Write-Error "No Webserver, or pwd received"
-            $password = $pwdFailsafe
-        }
-
-    Write-Host "process account..."
-    Write-Host "$GamDir\gam.exe create user $ReplacementUserMail firstname '$FirstName' lastname '$LastName' org '$GoogleWorkspaceDestinationUserOU' $GoogleCustomAttribute01 $HRid password $password gal $GoogleIncludeInGal changepasswordatnextlogin True"
-    #Invoke-Expression "$GamDir\gam.exe create user $ReplacementUserMail firstname '$FirstName' lastname '$LastName' org '$GoogleWorkspaceDestinationUserOU' $GoogleCustomAttribute01 $HRid password '$password' gal $GoogleIncludeInGal changepasswordatnextlogin True" ###create user #CID00#### dry run
-    #Start-Sleep 5 #allow time for user creation
-
-    #capture initial credentials
-    #"$firstname,$lastname,$legacyUserMail,$ReplacementUserMail,$password,$HRid" | out-file -filepath $tempcsv2 -Append
-
     #generate MFA backup codes
     #$userBackupCodes = invoke-expression "$GamDir\gam.exe user $ReplacementUserMail update backupcodes"
     Write-host "$GamDir\gam.exe user $ReplacementUserMail update backupcodes | ForEach-Object { $_ -replace '^\s*\d+:\s*', '' }"
     $userBackupCodes = Invoke-expression "$GamDir\gam.exe user $ReplacementUserMail update backupcodes" | ForEach-Object { $_ -replace '^\s*\d+:\s*', '' } #cleanup output
 
-   
     #send mail(s)
     ##backup codes...
     Write-Host "$GamDir\gam.exe sendemail $legacyUserMail from $GoogleWorkspaceSenderMail subject 'MFA Backup Codes as of $(get-Date)' message '$userBackupCodes'"
