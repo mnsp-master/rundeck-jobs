d5d5e030563deffb0c5fedfec1395ae0a57a9da7
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2026-03-20 08:11:43 +0000

    0.0.4

--- rundeck-cid004629-mass-import-users-google-azure-ad.ps1
+++ rundeck-cid004629-mass-import-users-google-azure-ad.ps1
@@ -1,7 +1,21 @@
-$mnspver = "0.0.3"
+$mnspver = "0.0.4"
+
+function DashedLine {
+Write-host "-----------------------------------------------------------`n"
+}
 
 Write-Host $(Get-Date)
 Write-Host "MNSP Version" $mnspver
 #Start-Sleep 10
 $ErrorActionPreference="Continue"
 Set-Location $GamDir
+
+#Set Google Instance: Destination...
+Write-Host "###### Set Google instance: Destination... ######"
+
+Write-Host "Setting workspace Destination: $GoogleWorkSpaceDestination"
+Invoke-Expression "$GamDir\gam.exe select $GoogleWorkSpaceDestination save" # swap/set google workspace
+Invoke-Expression "$GamDir\gam.exe"
+start-sleep 3
+DashedLine
+
