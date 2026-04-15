935afdef1bea50ede7e2bd7953a5ac520731aca8
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2026-04-15 12:58:53 +0100

    0.0.26_19_16_12

--- rundeck-cid04691-opencv-process-google-profile-images_python_01_win.ps1
+++ rundeck-cid04691-opencv-process-google-profile-images_python_01_win.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.26_19_16_11" #use python for all image coordinates
+$mnspver = "0.0.26_19_16_12" #use python for all image coordinates
 Clear-Host
 
 function DashedLine {
@@ -16,7 +16,7 @@ $dataout = "$datadir\output\$now"
 $passports = "$dataout\250x250"
 $exiftoolAppVersion = "exiftool-13.55_64" # https://exiftool.org/
 
-$photosSrc = $(Get-ChildItem -Path $datasrc ) # | Select-Object -ExpandProperty FullName)
+$photosSrc = $(Get-ChildItem -Path $datasrc )
 $ImgDimensions = @()
 $ImgDimensionX = @()
 $ImgDimensionY = @()
@@ -72,23 +72,11 @@ foreach ($photo in $photosSrc) {
 
     }
 
-
-    
-    # $ImgDimensionX, $ImgDimensionY = ( & identify -format "%w,%h" $filePath).Split(',') #get image dimensions #linx need win alternative [TODO]
-    # $ImgEXIFDateTimeOriginal = (& identify -format "%[EXIF:DateTimeOriginal]" $filePath) #get image creation date using EXIF #linx need win alternative [TODO]
-
-    # attempt detection using facedetect
-    #$imgCoordinates = ( & facedetect $filePath --best) # get bounding box of face
-
-    # if facedetect fails, trigger python fallback
-        
-                #Write-Warning "No coordinates found for image: $filePath" #no face detected in source image
-                #Write-Host "Trying alternative python method using: $pythonScriptName"
                 
                 #generate image with bounding box,center and csv data of metadata for image...
                 & python "$workDir\$pythonScriptName" $filePath $dataOut
 
-                #set coordinates from python processing...[IN PROGRESS]
+                #set coordinates from python processing...
                 #check for csv (none produced if no faces detected)
                 if (Test-path -path $dataout\$FileBaseName.csv) {
                 $pythonCoords = import-csv -path $dataout\$FileBaseName.csv #update to use Variable(s) 
@@ -101,49 +89,86 @@ foreach ($photo in $photosSrc) {
                     continue
                 }
 
-                #Write-Host "Python determined Unit value:" $Unit 
         #only proceed if facial detection confidence is above 0.7 %
         $faceDetectionScore = $PythonCoords.confidence
         if ($faceDetectionScore -ge 0.7) {
             Write-Host "Face detected with confidence:" $faceDetectionScore
+            Write-Host "Image Dimension X     :" $ImgDimensionX
+            Write-Host "Image Dimension Y     :" $ImgDimensionY
+            Write-Host "EXIF DateTimeOriginal :" $ImgEXIFDateTimeOriginal
+            Write-Host "Square Unit           :" $unit
+                
+            #$OriginTop = $($imgCoordinates.Split(" ")[1] -$unit) #new Horizontal Origin Value
+            $OriginTop = $($pythonCoords.CenterY - ($unit * 2)) #new Horizontal Origin Value
+            Write-Host "New Horizontal Origin :" $OriginTop
+                
+            #$OriginLeft = $($imgCoordinates.Split(" ")[0] -$unit) #minus full unit
+            $OriginLeft = $($pythonCoords.CenterX - ($unit *2))
+            Write-Host "New Vertical Origin   :" $OriginLeft
+                
+            #$Coords = $($imgCoordinates.Split(" ")[2] )
+            $Coords = $($unit * 2)
+                
+            #$CoordXY = ([int]$Coords * 2)
+            $CoordXY = ($unit * 4)
+                
+            Write-Host "New XY Coordinate     :" $CoordXY
+            Write-Host "Output Image          : " $dataout\$fileName
 
-            #Write-Host "Face detected by facedetect Processing image..."
-            #$imgCoordinatesCentre = ( & facedetect $filePath --best -c) # get center of image co-ordinates
-            
-            #if ($null -ne $imgCoordinates) { #check for detected face in source image
-                
-                Write-Host "Image Dimension X     :" $ImgDimensionX
-                Write-Host "Image Dimension Y     :" $ImgDimensionY
-                Write-Host "EXIF DateTimeOriginal :" $ImgEXIFDateTimeOriginal
-                #Write-Host "Derived Face coords   :" $imgCoordinates
-                #Write-Host "Derived face Center   :" $imgCoordinatesCentre
-                
-                #unit - detected bounding box divided in half
-                #$unit = $([Math]::Round($imgCoordinates.Split(" ")[3]/2)) # divide one of square values in half, rounded up to next whole number
-                Write-Host "Square Unit           :" $unit
-                
-                #$OriginTop = $($imgCoordinates.Split(" ")[1] -$unit) #new Horizontal Origin Value
-                $OriginTop = $($pythonCoords.CenterY - ($unit * 2)) #new Horizontal Origin Value
-                Write-Host "New Horizontal Origin :" $OriginTop
-                
-                #$OriginLeft = $($imgCoordinates.Split(" ")[0] -$unit) #minus full unit
-                $OriginLeft = $($pythonCoords.CenterX - ($unit *2))
-                Write-Host "New Vertical Origin   :" $OriginLeft
-                
-                #$Coords = $($imgCoordinates.Split(" ")[2] )
-                $Coords = $($unit * 2)
-                
-                #$CoordXY = ([int]$Coords * 2)
-                $CoordXY = ($unit * 4)
-                
-                Write-Host "New XY Coordinate     :" $CoordXY
-                Write-Host "Output Image          : " $dataout\$fileName
-
-                if ($OriginTop -lt 0 -or $OriginLeft -lt 0) {
+            if ($OriginTop -lt 0 -or $OriginLeft -lt 0) {
                 Write-Warning "One or more Origin values: $OriginTop $OriginLeft are negative, consider providing a better image..."
-                }
+            }
+            
+            #remove background from source image first...
+            $TMPIMG1 = "${fileBaseName}_$(Get-Date -Format HHmmss)"  #temporary unique filename
+            
+            # use pyton library rembg to replace background with solid white
+            & rembg i -m u2net -bgc 255 255 255 255 -a -ae 5 $FilePath $dataOut/$TMPIMG1.png
 
-                #NOTE: can fail to give 1:1 ratio image under some source image secnarios...
+            #replaces transparent bg with solid white
+            & $WorkDir\ImageMagick\magick.exe $dataout\$TMPIMG1.png -background white -alpha remove -alpha off $dataout/$fileName 
+            
+            #center produced image
+            & $WorkDir\ImageMagick\magick.exe $dataout\$TMPIMG1.png -crop "${CoordXY}x${CoordXY}+$OriginLeft+$OriginTop" +repage -gravity center -extent "${CoordXY}x${CoordXY}" "$dataout/$fileName"
+            
+            #produce 250x250 pixel image in $passports directory
+            & $WorkDir\ImageMagick\magick.exe $dataout\$fileName -resize 250x250 $passports/$fileName
+            remove-item $dataout\$TMPIMG1.png -force -verbose # delete temp file
+                
+            Start-Sleep 1
+            DashedLine
+        } else {
+            Write-Warning "No Face detected for file: $filePath"
+    }
+}
+
+<#
+Write-Host "Cleaning up temporary files..."
+remove-item $dataout\*.csv -force -verbose
+remove-item $dataout\detected*.* -force -verbose
+#>
+
+Stop-Transcript
+
+<#
+#& convert $dataout/$TMPIMG1.png -crop "${CoordXY}x${CoordXY}+$OriginLeft+$OriginTop" +repage -gravity center -background white -extent "${CoordXY}x${CoordXY}" "$dataout/$fileName"
+#$ImgDimensions = $((Invoke-Expression "identify $filePath").Split(" ")[2])
+    #$ImgDimensionX = $($ImgDimensions.Split("x")[0])
+    #$ImgDimensionY = $($ImgDimensions.Split("x")[1])
+
+    #$imgCoordinates = $(Invoke-Expression "facedetect $filePath --best")
+    #$imgCoordinatesCentre = $(Invoke-Expression "facedetect $filePath --best -c")
+
+    #$OriginLeft = $($imgCoordinates.Split(" ")[0] -$unit/2)#minus half unit
+
+    #$CoordXY
+        #$CoordX = $($OriginLeft + ($unit * 4))
+        #$CoordY = $($OriginTop + ($unit * 4))
+        #Write-Host "New Y Coordinate     :" $CoordY
+    #Invoke-expression "convert $filePath -crop $CoordXY$convertX$CoordXY+$OriginLeft+$OriginTop $dataout/$fileName"
+    #Invoke-Expression "convert $dataout/$fileName -resize 250x250 $passports/$fileName"
+
+        #NOTE: can fail to give 1:1 ratio image under some source image secnarios...
                 #& convert $filePath -crop $CoordXY$convertX$CoordXY+$OriginLeft+$OriginTop $dataout/$fileName
 
                 <# original process
@@ -160,47 +185,4 @@ foreach ($photo in $photosSrc) {
                 remove-item $dataout/$TMPIMG1.png -force -verbose # delete temp file
                 #>
 
-                #remove background from source image first...
-                $TMPIMG1 = "${fileBaseName}_$(Get-Date -Format HHmmss)"  #temporary unique filename
-                #& rembg i $FilePath $dataOut/$TMPIMG1.png # use pyton library rembg to remove background
-                & rembg i -m u2net -bgc 255 255 255 255 -a -ae 5 $FilePath $dataOut/$TMPIMG1.png # use pyton library rembg to replace background with solid white
-
-                & $WorkDir\ImageMagick\magick.exe $dataout\$TMPIMG1.png -background white -alpha remove -alpha off $dataout/$fileName #replaces transparent bg with solid white
-                #& convert $dataout/$TMPIMG1.png -crop "${CoordXY}x${CoordXY}+$OriginLeft+$OriginTop" +repage -gravity center -background white -extent "${CoordXY}x${CoordXY}" "$dataout/$fileName"
-                & $WorkDir\ImageMagick\magick.exe $dataout\$TMPIMG1.png -crop "${CoordXY}x${CoordXY}+$OriginLeft+$OriginTop" +repage -gravity center -extent "${CoordXY}x${CoordXY}" "$dataout/$fileName"
-                & $WorkDir\ImageMagick\magick.exe $dataout\$fileName -resize 250x250 $passports/$fileName #produce 250x250 pixel image in $passports directory
-                remove-item $dataout\$TMPIMG1.png -force -verbose # delete temp file
-
-                
-                Start-Sleep 1
-            DashedLine
-        } else {
-            Write-Warning "No Face detected for file: $filePath"
-    }
-}
-
-<#
-Write-Host "Cleaning up temporary files..."
-remove-item $dataout\*.csv -force -verbose
-remove-item $dataout\detected*.* -force -verbose
-#>
-
-Stop-Transcript
-
-<#
-#$ImgDimensions = $((Invoke-Expression "identify $filePath").Split(" ")[2])
-    #$ImgDimensionX = $($ImgDimensions.Split("x")[0])
-    #$ImgDimensionY = $($ImgDimensions.Split("x")[1])
-
-    #$imgCoordinates = $(Invoke-Expression "facedetect $filePath --best")
-    #$imgCoordinatesCentre = $(Invoke-Expression "facedetect $filePath --best -c")
-
-    #$OriginLeft = $($imgCoordinates.Split(" ")[0] -$unit/2)#minus half unit
-
-    #$CoordXY
-        #$CoordX = $($OriginLeft + ($unit * 4))
-        #$CoordY = $($OriginTop + ($unit * 4))
-        #Write-Host "New Y Coordinate     :" $CoordY
-    #Invoke-expression "convert $filePath -crop $CoordXY$convertX$CoordXY+$OriginLeft+$OriginTop $dataout/$fileName"
-    #Invoke-Expression "convert $dataout/$fileName -resize 250x250 $passports/$fileName"
 #>
