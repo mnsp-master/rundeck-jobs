22c62a54f49578831894b32f1dfa447e63ffcc05
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2026-04-16 09:09:35 +0100

    0.0.26_19_16_24

--- rundeck-cid04691-opencv-process-google-profile-images_python_01_win.ps1
+++ rundeck-cid04691-opencv-process-google-profile-images_python_01_win.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.26_19_16_23" #use python for all image coordinates
+$mnspver = "0.0.26_19_16_24" #use python for all image coordinates
 Clear-Host
 
 function DashedLine {
@@ -91,6 +91,14 @@ foreach ($photo in $photosSrc) {
 
         #only proceed if facial detection confidence is above $ConfidenceLevel %
         $faceDetectionScore = $PythonCoords.confidence
+        #some images return multiple confidence values - ignore these
+        $values = $faceDetectionScore.split(" ") | where-object { $_ -match "^\d+\.\d+$" }
+        if ($values.count -gt 1) {
+            Write-Warning "Multipe Values detected for $fileName ..."
+            Continue
+        }
+
+
         if ($faceDetectionScore -ge $ConfidenceLevel ) {
             Write-Host "Face detected with confidence:" $faceDetectionScore
             Write-Host "Image Dimension X     :" $ImgDimensionX
