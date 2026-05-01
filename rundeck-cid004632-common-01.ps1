873a7448239bad3e1396cb63bdf0e4e2ad7ce7f6
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2026-05-01 12:57:44 +0100

    0.0.24

--- rundeck-cid004632-common-01.ps1
+++ rundeck-cid004632-common-01.ps1
@@ -1,5 +1,5 @@
 Clear-Host
-$mnspver = "0.0.23"
+$mnspver = "0.0.24"
 
 
 
@@ -121,7 +121,7 @@ $emailBody = @"
 
 #create credential object to authenticate to smtp 
 [SecureString]$securepassword = $GWSpassword | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $securepassword
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $GWSusername, $securepassword
 
 #copy contents of transcript log to temp txt file to use as attachment, as log will be in use, and cannot be directly attached...
 $transcriptlogCopy = $(Get-Content $transcriptlog | set-content $transcriptlogTemp)
