5fd62784f5ed10e3abc713b3fbb8215690f78491
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2026-05-01 13:04:02 +0100

    0.0.7

--- rundeck-cid004763-set-ad-gws-azure-pwd.ps1
+++ rundeck-cid004763-set-ad-gws-azure-pwd.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.6"
+$mnspver = "0.0.7"
 
 Function Get-NewPassword {
     $PwdUrl = $MNSPgetPasswordURL
@@ -40,7 +40,7 @@ Write-Host "MNSP script: $scriptName version: $mnspver"
 
 
 $password = Get-NewPassword
-Write-Host "Confirm Password function: $password"
+#Write-Host "Confirm Password function: $password"
 $RundeckJobOutput = "New User password: $password"
 
 DashedLine
