1845a31b7501b09508f947506d46c6eea4069c94
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2026-05-01 11:31:25 +0100

    0.0.1

---
+++ rundeck-cid004763-set-ad-gws-azure-pwd.ps1
@@ -0,0 +1,46 @@
+$mnspver = "0.0.1"
+
+Function Get-NewPassword {
+    $PwdUrl = $MNSPgetPasswordPRL
+
+    #failsafe password
+    $pwdFailsafe = $MNSPgetPasswordPRLfailsafe
+
+    
+    try {
+        Write-Host "Attempting to retrieve password from web service..."
+        $response = Invoke-WebRequest -Uri $PwdUrl -UseBasicParsing
+
+        # Extract the content from the response object and store it in a variable.
+        $password = $response.Content
+
+        # Return the generated password.
+        return $password
+    }
+    catch {
+        # If the web request fails, a detailed error message is logged.
+        Write-Error "Failed to retrieve password from $PwdUrl. Using failsafe password instead." -ErrorAction Stop
+
+        # Return the failsafe password as the function's output.
+        return $pwdFailsafe
+    }
+}
+
+
+
+Write-Host $(Get-Date)
+Write-Host "MNSP Version" $mnspver
+
+$ScriptName = Split-Path $PSCommandPath -Leaf
+Write-Host "Executing Main Rundeck Job..."
+Write-Host "MNSP script: $scriptName version: $mnspver"
+
+#Start-Sleep 10
+$ErrorActionPreference="Continue"
+
+
+$password = Get-NewPassword
+Write-Host "Confirm Password function: $password"
+
+DashedLine
+
